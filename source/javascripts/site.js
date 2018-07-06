9b63252a8ccd7329e6c47dedc2c65fce1d58e2e8
stevenyoungko <stevenke60815@gmail.com>
2018-07-06 20:02:43 +0800

    Modify header bg and js (navMenuOnScrollEffective)

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -1,32 +1,33 @@
-var buttons = document.querySelectorAll('[data-scroll-to]')
+// 導覽列按鈕點擊可以移動畫面
+var initSmoothScrollTo = function() {
+  var buttons = document.querySelectorAll('[data-scroll-to]')
 
-buttons.forEach(function(element) {
+  buttons.forEach(function (element) {
 
-  element.addEventListener('click', event => {
-    var target = event.currentTarget.dataset.scrollTo;
-    var offset = document.querySelector(target).offsetTop;
-    
-    window.scroll({
-      top: offset,
-      behavior: 'smooth'
+    element.addEventListener('click', event => {
+      var target = event.currentTarget.dataset.scrollTo;
+      var offset = document.querySelector(target).offsetTop;
+
+      window.scroll({
+        top: offset,
+        behavior: 'smooth'
+      })
     })
   })
-})
+}
 
-$(function() {
-
-  var duration = 300 
-  
+// 手機版選單滑入效果
+var popupMenuMoveToWindow = function() {
+  var duration = 300
   var $menu = $('.popup-menu')
   var $button = $('.navbar-burger')
-    .on('click', function() {
+    .on('click', function () {
       $menu.toggleClass('open')
-      if($menu.hasClass('open')) {
+      if ($menu.hasClass('open')) {
         $menu.stop(true).animate({
           left: '0'
         }, duration)
-      }
-      else{
+      } else {
         $menu.stop(true).animate({
           left: '-100%'
         }, duration)
@@ -34,31 +35,135 @@ $(function() {
     })
 
   var buttons = $('.popup-menu-item').on('click', MoveToLeft)
-  
   var close = $('.popup-menu-close').on('click', MoveToLeft)
 
-  function MoveToLeft () {
+  function MoveToLeft() {
     $menu.animate({
       left: '-100%'
     }, duration)
     $menu.removeClass('open')
   }
-})
+}
 
-// var number = $('[data-number]')
-// console.log(number)
-var button = $('[data-scroll-to]')
-button.on('click',function(event) {
-  var numAnim = new CountUp("counter", 0, 50);
-  if (!numAnim.error) {
-    numAnim.start();
-  } else {
-    console.error(numAnim.error);
+//畫面移入計時器開始
+var viewPortCountUp = function () {
+  $.fn.isInViewport = function () {
+    var elementTop = $(this).offset().top;
+    var elementBottom = elementTop + $(this).outerHeight();
+    var viewportTop = $(window).scrollTop();
+    var viewportBottom = viewportTop + $(window).height();
+    return elementBottom > viewportTop && elementTop < viewportBottom;
+  };
+
+  var isCounted = false
+
+  $(window).on('resize scroll', function () {
+
+    var counter = $('.patents-up')
+
+    if (counter.length === 0) return
+    // console.log(isCounted)
+
+    if ($('.patents-up').isInViewport()) {
+      if (isCounted) return
+
+      var numAnim = new CountUp("counter", 0, 50);
+
+      if (numAnim.error) return
+
+      numAnim.start()
+      isCounted = true
+
+    } else {
+      if (!isCounted) return
+
+      isCounted = false
+    }
+  });
+}
+
+//手機版選單active效果
+var popupMenuActive = function () {
+  var button = $('.popup-menu-item')
+  button.on('click', function () {
+    $('.popup-menu-list a').removeClass('is-active')
+    $(this).addClass('is-active')
+  })
+}
+
+// product頁面的背景效果
+var particlesEffective = function () {
+  window.onload = _.debounce(function () {
+    if (!$('.background').length > 0) return
+    Particles.init({
+      selector: '.background',
+      connectParticles: true,
+      color: ["#FFBB66", "#EEEE00", "#FF0000", "#FFFFFF"],
+      maxParticles: 130,
+      sizeVariations: 5,
+    });
+  },100)
+}
+
+//導覽列滑動效果
+var navMenuOnSrollEffective = function () {
+  var navMenuItems = $('.navbar-item[data-scroll-to]')
+
+  var activatedNavMenuLine = function (apple) {
+    $('.navbar-menu').find('.active').removeClass('active')
+    apple.addClass('active')
   }
-  $('.popup-menu-list a').removeClass('is-active')
-  $(this).addClass('is-active')
+
+  $.each(navMenuItems, function (index, element) {
+
+    var itemTarget = element.dataset.scrollTo
+    var target = $(itemTarget)
+    var targetHeight = target.outerHeight()
+    var targetOffsetTop = target.offset().top
+    var navbarItem = $(element)
+    // var line = $(element).find('.line')
+    var headerHeight = $('.header').outerHeight()
+
+    $(window).on('scroll', _.debounce(function () {
+      
+      var currentScrollDistance = $(window).scrollTop() + headerHeight
+      // console.log(currentScrollDistance)
+      // if (index === 0 && currentScrollDistance >= headerHeight) {
+      //   activatedNavMenuLine(line)
+      // }
+      if (currentScrollDistance >= targetOffsetTop && currentScrollDistance <= targetHeight + targetOffsetTop) {
+        activatedNavMenuLine(navbarItem)
+      }
+    }, 100))
+  })
+}
+
+$(function () {
+  initSmoothScrollTo()
+  popupMenuMoveToWindow()
+  viewPortCountUp()
+  popupMenuActive()
+  particlesEffective()
+  navMenuOnSrollEffective()
 })
 
+// var patentsCountUp = function() {
+//   var button = $('.popup-menu-item')
+//   button.on('click', function (event) {
+//     var numAnim = new CountUp("counter", 0, 50);
+//     if (!numAnim.error) {
+//       numAnim.start();
+//     } else {
+//       console.error(numAnim.error);
+//     }
+//   })
+// }
+
+// var $target = $('.navbar-item')
+// $target.on('click',function() {
+//   $('.navbar-item div').removeClass('active')
+//   $(this).find('.line').addClass('active')
+// })
 
 // $(function() {
 //   $(window).on('scroll', function () {
@@ -75,48 +180,78 @@ button.on('click',function(event) {
 //   })
 // })
 
-$.fn.isInViewport = function () {
-  var elementTop = $(this).offset().top;
-  var elementBottom = elementTop + $(this).outerHeight();
 
-  var viewportTop = $(window).scrollTop();
-  var viewportBottom = viewportTop + $(window).height();
+// $(window).on('resize scroll', function() {
+//   if ($('.section-page').isInViewport()) {
+//     $('.navbar-menu a').removeClass('active')
+//     $('.navbar-item').addClass('active')
+//   }
+// })
 
-  return elementBottom > viewportTop && elementTop < viewportBottom;
-};
+// var homeLine = $('[data-scroll-to="#home"]').find($('.line'))
+// var technologyLine = $('[data-scroll-to="#technology"]').find($('.line'))
+// var visionLine = $('[data-scroll-to="#vision"]').find($('.line'))
+// var productLine = $('[data-scroll-to="#products"]').find($('.line'))
+// var contactLine = $('[data-scroll-to="#contact-us"]').find($('.line'))
+// var home = $('#home')
+// var technology = $('#technology')
+// var vision = $('#vision')
+// var product = $('#products')
+// var contact = $('#contact-us')
+// $(window).on('scroll', function () {
+//   var header = $('.header').outerHeight()
+//   var scrollDistance = $(window).scrollTop() + header
+//   var homeHeight = home.outerHeight()
+//   var technologyTop = technology.offset().top
+//   var visionTop = technologyTop + technology.outerHeight() 
+//   var productTop = visionTop + vision.outerHeight()
+//   var contactTop = productTop + product.outerHeight() 
 
-var isCounted = false
+//   if (header <= scrollDistance) {
+//     homeLine.addClass('active')
+//   }
 
-$(window).on('resize scroll', function () {
+//   if (homeHeight <= scrollDistance) {
+//     homeLine.removeClass('active')
+//   }
+  
+//   if (technologyTop <= scrollDistance) {
+//     technologyLine.addClass('active')
+//   }
 
-  console.log(isCounted)
+//   if (visionTop <= scrollDistance) {
+//     technologyLine.removeClass('active')
+//     visionLine.addClass('active')
+//   }
 
-  if ($('.patents-up').isInViewport()) {
-    if (isCounted) return
+//   if (scrollDistance <= technologyTop) {
+//     technologyLine.removeClass('active')
+//   }
 
-    var numAnim = new CountUp("counter", 0, 50);
+//   if (productTop <= scrollDistance) {
+//     visionLine.removeClass('active')
+//     productLine.addClass('active')
+//   }
 
-    if (numAnim.error) return
+//   if (scrollDistance <= visionTop) {
+//     visionLine.removeClass('active')
+//   }
 
-    numAnim.start()
-    isCounted = true
+//   if (scrollDistance <= productTop) {
+//     productLine.removeClass('active')
+//   }
 
-  } else {
-    if (!isCounted) return
+//   if (scrollDistance <= contactTop) {
+//     contactLine.removeClass('active')
+//   }
 
-    isCounted = false
-  }
-});
+//   if (contactTop <= scrollDistance) {
+//     productLine.removeClass('active')
+//     contactLine.addClass('active')
+//   }
+// })
 
-window.onload  = function () {
-  Particles.init({
-    selector: '.background',
-    connectParticles: true,
-    color: ["#FFBB66", "#EEEE00", "#FF0000", "#FFFFFF"],
-    maxParticles: 130,
-    sizeVariations: 5,
-  });
-};
+
 
 
 
